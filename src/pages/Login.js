e28394e1d9ad001a098ab62bf308bf167e9f627d
akanksha9975 <akankshayadav9975@gmail.com>
2025-04-05 16:55:22 +0530

    new commit

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -77,15 +77,8 @@ function Login() {
           </form>
         </div>
         
-        <div className="login-pagination">2of4</div>
       </div>
       
-      <button className="nav-arrow left-arrow" onClick={() => navigate('/')}>
-        <FaArrowLeft />
-      </button>
-      <button className="nav-arrow right-arrow" onClick={() => navigate('/create-account')}>
-        <FaArrowRight />
-      </button>
     </div>
   );
 }
